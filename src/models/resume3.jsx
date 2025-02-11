a02085f8475af6eb745b4af1fe86c127c3baac7b
Chrimika <mbachristian58@gmail.com>
2025-02-11 11:40:42 +0100

    Resume3 modele de cv 3

---
+++ src/models/resume3.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import image from '../assets/image.jpg'
+
+const Resume3 = () => {
+    return(
+    <div style={{flex:1,display:"flex",margin:'0 auto',height:'100vh',flexDirection:'column',justifyContent:'center',maxWidth:700,alignItems:'center'}}>
+        <div style={{flex:0.2,width:'100%',display:'flex'}}>
+            <div style={{flex:0.35,padding:8,backgroundColor:'#282831',display:'flex',justifyContent:'center',alignItems:'flex-end'}}>
+                <div style={{border:'1px solid white',flex:1,marginRight:-8,height:'100%',borderWidth:'8px 0 8px 8px',backgroundColor:'#19909e',display:'flex',justifyContent:'center',padding:8}}>
+                    <div style={{width:100,height:100,borderRadius:50,backgroundColor:'white',aspectRatio: 1, backgroundImage: `url(${image})`,backgroundSize: 'cover'}}>
+
+                    </div>
+                </div>
+            </div>
+            <div style={{flex:0.65,padding:8}}>
+                <div style={{border:'1px solid white',flex:1,height:'100%',borderWidth:'8px 8px 8px 0',backgroundColor:'#19909e',marginLeft:-8,padding:8}}>
+                    <h1 style={{fontSize:'1.5rem',fontWeight:'bold',color:'white'}}>Mika MBA</h1>
+                    <p style={{fontSize:'0.6rem',whiteSpace: 'normal',wordWrap: 'break-word',color:'white'}}>mbachristian58@gmail.com</p>
+                    <p style={{fontSize:'0.6rem',whiteSpace: 'normal',wordWrap: 'break-word',color:'white'}}>+237 672 094 167</p>
+                    <p style={{fontSize:'0.6rem',whiteSpace: 'normal',wordWrap: 'break-word',color:'white'}}>3 ans dexperience</p>
+                    <p style={{fontSize:'0.6rem',whiteSpace: 'normal',wordWrap: 'break-word',color:'white'}}>permis B</p>
+                </div>
+            </div>
+        </div> 
+        <div style={{flex:0.8,width:'100%',display:'flex'}}>
+            <div style={{flex:35,backgroundColor:'#282831',padding:8,display:'flex',flexDirection:'column'}}>
+                <div style={{marginTop:8,flex:1,display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',}}>
+                    <div style={{marginTop:8,flex:1,display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',width:'100%'}}>
+                        <div style={{flex:0.2,width:'100%',marginBottom:4}}>
+                            <p style={{color:'#b2c1c3',letterSpacing:1.3}}>Compétences</p>
+                        </div>
+                        <div style={{flex:0.8,width:'100%', overflow: 'hidden',display:'flex',flexDirection:'column'}}>
+                            <p style={{fontSize:'0.6rem',whiteSpace: 'normal',wordWrap: 'break-word',color:'white'}}>microsoft office</p>
+                            <p style={{fontSize:'0.6rem',whiteSpace: 'normal',wordWrap: 'break-word',color:'white'}}>React Js</p>
+                            <p style={{fontSize:'0.6rem',whiteSpace: 'normal',wordWrap: 'break-word',color:'white'}}>React native</p>
+                        </div>
+                    </div>
+                    <div style={{marginTop:8,flex:1,display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',width:'100%'}}>
+                        <div style={{flex:0.2,width:'100%',marginBottom:4}}>
+                            <p style={{color:'#b2c1c3',letterSpacing:1.3}}>Langues</p>
+                        </div>
+                        <div style={{flex:0.8,width:'100%', overflow: 'hidden',display:'flex',flexDirection:'column'}}>
+                            <p style={{fontSize:'0.6rem',whiteSpace: 'normal',wordWrap: 'break-word',color:'white'}}>Anglais</p>
+                            <p style={{fontSize:'0.6rem',whiteSpace: 'normal',wordWrap: 'break-word',color:'white'}}>Francais</p>
+                        </div>
+                    </div>
+                    <div style={{marginTop:8,flex:1,display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',width:'100%'}}>
+                        <div style={{flex:0.2,width:'100%',marginBottom:4}}>
+                            <p style={{color:'#b2c1c3',letterSpacing:1.3}}>Centres D'interet</p>
+                        </div>
+                        <div style={{flex:0.8,width:'100%', overflow: 'hidden',display:'flex',flexDirection:'column'}}>
+                            <p style={{fontSize:'0.6rem',whiteSpace: 'normal',wordWrap: 'break-word',color:'white'}}>Anglais</p>
+                            <p style={{fontSize:'0.6rem',whiteSpace: 'normal',wordWrap: 'break-word',color:'white'}}>Francais</p>
+                        </div>
+                    </div>
+                </div>
+                <div style={{flex:0.3}}>
+
+                </div>
+            </div>  
+            <div style={{flex:65, padding:8,display:'flex',flexDirection:'column'}}>
+                <div style={{flex:0.2}}>
+                    <div style={{marginTop:8,flex:1,display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',}}>
+                        <div style={{flex:0.2,width:'100%',marginBottom:4,backgroundColor:'#19909e'}}>
+                            <p style={{color:'#fff',marginLeft:8}}>Profile</p>
+                        </div>
+                        <div style={{flex:0.8,width:'100%', overflow: 'hidden',display:'flex',flexDirection:'column'}}>
+                            <p style={{
+                            fontSize: '0.6rem',
+                            whiteSpace: 'normal',
+                            wordWrap: 'break-word'
+                            }}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.  
+                                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  
+                            </p>
+                        </div>
+                    </div>
+                    <div style={{marginTop:8,flex:1,display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',}}>
+                        <div style={{flex:0.2,width:'100%',marginBottom:4,backgroundColor:'#19909e'}}>
+                            <p style={{color:'#fff',marginLeft:8}}>Formation</p>
+                        </div>
+                        <div style={{flex:0.8,width:'100%', overflow: 'hidden',display:'flex',flexDirection:'column'}}>
+                            <p style={{
+                            fontSize: '0.6rem',
+                            whiteSpace: 'normal',
+                            wordWrap: 'break-word'
+                            }}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.  
+                                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  
+                            </p>
+                        </div>
+                    </div>
+                    <div style={{marginTop:8,flex:1,display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',}}>
+                        <div style={{flex:0.2,width:'100%',marginBottom:4,backgroundColor:'#19909e'}}>
+                            <p style={{color:'#fff',marginLeft:8}}>Experience Professionnelle</p>
+                        </div>
+                        <div style={{flex:0.8,width:'100%', overflow: 'hidden',display:'flex',flexDirection:'column'}}>
+                            <p style={{
+                            fontSize: '0.6rem',
+                            whiteSpace: 'normal',
+                            wordWrap: 'break-word'
+                            }}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.  
+                                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </div>         
+    </div>
+    )
+}
+
+export default Resume3;
--- src/pages/TestResume.jsx
+++ src/pages/TestResume.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Resume1 from '../models/resume1'
 import Resume2 from '../models/resume2'
+import Resume3 from '../models/resume3'
 
 const TestResume = () => {
   return (
     <div>
       <Resume2/>
       <Resume1/>
+      <Resume3/>
     </div>
 
   )
